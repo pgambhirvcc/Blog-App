5adf14fbba8d95357e5a680975fe4abc2766da73
Prabhjyot Gambhir <pgambhir@vcc.ca>
2025-05-30 17:46:04 -0700

    feature/getAllUsersApi

--- Routes/user.js
+++ Routes/user.js
@@ -8,6 +8,8 @@ router.post('/login', UserController.loginUser);
 
 router.get('/all', UserController.getAllUsers);
 
+router.get('/:id', UserController.getUserById);
+
 // API --> app.js (It is smart enough where to route) --> USer routes (smart enough what purpose) --> controller(registerUser)
 
 module.exports = router;
